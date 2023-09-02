2a66cca8465409d912f6ab2fe67953274b7b0cb3
Mark Stevens <mark@thepcsite.co.uk>
2023-09-02 13:55:14 +0100

    Can build debug versions of application for VS Code.

--- build.sh
+++ build.sh
@@ -2,15 +2,40 @@
 
 SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
+clean() {
+    make -j -C $SCRIPT_DIR/build clean
+    rm -R $SCRIPT_DIR/build/*
+    cmake -S . -B $SCRIPT_DIR/build
+}
+
 HELP=false
+DEBUG=false
+RELEASE=false
 for i in "$@"
 do
 case $i in
     -h|--help)
     HELP=true
     ;;
+    -c|--clean)
+    clean
+    ;;
+    -d|--debug)
+    if $RELEASE; then
+        echo "Cannot build both debug and release versions."
+        exit 1
+    fi
+    DEBUG=true
+    ;;
+    --release)
+    if $DEBUG; then
+        echo "Cannot build both debug and release versions."
+        exit 1
+    fi
+    RELEASE=true
+    ;;
     -r|--rebuild)
-    make -C $SCRIPT_DIR/build clean
+    clean
     ;;
     *)
     echo "${0##*/} - Unknown option $i"
@@ -20,12 +45,23 @@ esac
 done
 
 if $HELP; then
-    echo "Usage: ${0##*/} [-h|--help] [-r|--rebuild]"
+    echo "Usage: ${0##*/}"
     echo ""
     echo "Options:"
     echo "-h | --help             Display this message"
-    echo "-r | --rebuild          Remove any built objects and rebuild the project."
+    echo "-c | --clean            Clean the build directory"
+    echo "-d | --debug            Build a debug version of the application"
+    echo "--release               Build a release version of the application"
+    echo "-r | --rebuild          Remove any built objects and rebuild the project"
     exit 0
 fi
 
-cd $SCRIPT_DIR/build && make 
+if $DEBUG; then
+    cmake -DCMAKE_BUILD_TYPE=Debug -S . -B $SCRIPT_DIR/build
+fi
+
+if $RELEASE; then
+    cmake -S . -B $SCRIPT_DIR/build
+fi
+
+make -j -C $SCRIPT_DIR/build
